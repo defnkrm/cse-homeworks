c377086c0a5497753b01c89e70ccec1c5fdfd40a
Defne Demir <145259063+defnkrm@users.noreply.github.com>
2024-09-26 12:14:34 +0300

    Revert "2nd year, 1st semester (#1)"
    This reverts commit ead6f802ffc2ac2540831a742c8ab3b9cc1374e5.

--- HW01_20230808010.java
+++ HW01_20230808010.java
@@ -1,146 +1,132 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class HW01_20230808010 {
     public static void main(String[] args) {
-        TrafficControl lamba=new TrafficControl(5,5);
-        System.out.println(lamba);
-        System.out.println(lamba.countMalfunctioningLights());
-        System.out.println(lamba.crucialIntersections().get(0)[0]);
-        System.out.println(lamba.crucialIntersections().get(0)[1]);
-
-        int[] array=new int[12];
-
     }
 }
-interface ITrafficControl{
-    int[][] getCityGrid();
-    int countMalfunctioningLights();
-    String mostMalfunction();
-    int countMalfunctioningNeighbors(int row,int col);
-    List<int[]> crucialIntersections();
-    String toString();
+class Stock{
+   private String symbol; 
+   private String name;
+   private double previousClosingPrice;
+   private double currentPrice;
+
+
+   public double getCurrentPrice() {
+       return currentPrice;
+   }
+   public String getName() {
+       return name;
+   }
+   public double getPreviousClosingPrice() {
+       return previousClosingPrice;
+   }
+   public String getSymbol() {
+       return symbol;
+   }
+   
+   public void setPreviousClosingPrice(double previousClosingPrice) {
+       this.previousClosingPrice = previousClosingPrice;
+   }
+
+   public double setCurrentPrice(double newPrice){
+    if(newPrice<0){
+        System.out.println("ERROR : you have entered an invalid value.");
+    }
+    else{
+        previousClosingPrice=currentPrice;
+        currentPrice=newPrice;
+    }
+    return currentPrice;
+   }
+
+   public double getChangePercent(){
+    double percent=(currentPrice-previousClosingPrice)/previousClosingPrice;
+    return percent*100;
+   }
+
+   Stock(){
+
+   }
+
+   Stock(String givenSymbol,String givenName){
+    symbol=givenSymbol.toUpperCase();
+    String[] names=givenName.split(" ");
+    name=" ";
+    for (int i = 0; i < names.length; i++) {
+        name+=names[i].substring(0,1).toUpperCase()+names[i].substring(1).toLowerCase()+" ";
+    }
+    name=name.trim();
+   }
+
+   //@Override
+   public String toString() {
+       return String.format("[%s] - [%s]: [%f]",symbol,name,currentPrice);
+   }
+}
+
+class Fan{
+    private final int SLOW=1;
+    private final int MEDIUM=2;
+    private final int FAST=3;
+    private int speed;
+    private boolean on;
+    private double radius; 
+    private String color;
+
+    Fan(){
+        this(5,"Blue");
+        this.speed=SLOW;
+        this.on=false;
+    }
+    Fan(int radius, String color){
+        this.radius=radius;
+        this.color=color;
     }
 
-class TrafficControl implements ITrafficControl{
-    int[][] cityGrid;
-    public TrafficControl(int m, int n){
-        int[][] temp=new int[m][n];
-        int tem;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                tem=(int)(Math.random()*3);
-                temp[i][j]=tem;
-            }
+    public String getColor() {
+        return color;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int newSpeed){
+        if(on){
+            if(newSpeed==1||newSpeed==2||newSpeed==3)
+                speed=newSpeed;
+            else
+                System.out.println("ERROR: You have entered an invalid speed.");
         }
-        this.cityGrid=temp;
     }
-    public TrafficControl(int[][] cityGrid){
-        this.cityGrid=cityGrid;
+    public void setRadius(double radius) {
+        this.radius = radius;
     }
-    @Override
-    public int[][] getCityGrid() {
-        return cityGrid;
+    public void setColor(String color) {
+        this.color = color;
     }
+    
 
-    @Override
-    public int countMalfunctioningLights() {
-    //returns how many 1's the grid has
-        int counter=0;
-        for (int i = 0; i < cityGrid.length; i++) {
-            for (int j = 0; j < cityGrid[0].length; j++) {
-                if(cityGrid[i][j]==1)
-                    counter++;
-            }
-        }
-        return counter;
+    public boolean isOn(){
+        if(on)
+            return true;
+        else 
+            return false;
     }
-
-    @Override
-    public String mostMalfunction() {
-        int[] rowMalfunctionCounter=new int[cityGrid[0].length];
-        int[] columnMalfunctionCounter=new int[cityGrid.length];
-        for (int i = 0; i < cityGrid.length; i++) {
-            for (int j = 0; j < cityGrid[0].length; j++) {
-                if(cityGrid[i][j]==1){
-                    rowMalfunctionCounter[i]++;
-                    columnMalfunctionCounter[j]++;
-                }
-            }
-        }
-        String result="";
-        int maxRow=0;
-        int maxCol=0;
-        int temp=rowMalfunctionCounter[0];
-        for (int i = 1; i < rowMalfunctionCounter.length; i++) {
-            if(rowMalfunctionCounter[i]>temp){
-                temp=rowMalfunctionCounter[i];
-                maxRow=i;
-            }
-        }
-        temp=columnMalfunctionCounter[0];
-        for (int i = 1; i < columnMalfunctionCounter.length; i++) {
-            if(columnMalfunctionCounter[i]>temp){
-                temp=columnMalfunctionCounter[i];
-                maxCol=i;
-            }
-        }
-        if(rowMalfunctionCounter[maxRow]>columnMalfunctionCounter[maxCol])
-            result+="Row: "+Integer.toString(maxRow);
-        else if (columnMalfunctionCounter[maxCol]>rowMalfunctionCounter[maxRow])
-            result+="Column: "+Integer.toString(maxCol);
+    public void change(){
+        if(on)
+            on=false;
         else
-            result+="Row: "+Integer.toString(maxRow)+" and Column: "+Integer.toString(maxCol)+" both have the most malfunctions.";
-        return result;
+            on=true;
     }
 
-    @Override
-    public int countMalfunctioningNeighbors(int row, int col) {
-        int count=0;
-        if(col-1 >= 0){
-            if(cityGrid[row][col-1] == 1)
-                count++;
+    //@Override
+    public String toString() {
+        if(on){
+            return String.format("Speed: %d,Radius %.2f, Color: %s",speed,radius,color);
         }
-        if(row-1 >= 0){
-            if(cityGrid[row-1][col] == 1)
-                count++;
-        }
-        if(row+1 < cityGrid.length){
-            if(cityGrid[row+1][col] == 1)
-                count++;
-        }
-        if(col+1 < cityGrid[0].length){
-            if(cityGrid[row][col+1] == 1)
-                count++;
-        }
-        return count;
+        else{
+            return String.format("Fan is off");
+        }        
+        
     }
-
-    @Override
-    public List<int[]> crucialIntersections() {
-        List<int[]> result=new ArrayList<>();
-
-        for (int i = 0; i < cityGrid.length; i++) {
-            for (int j = 0; j < cityGrid[0].length; j++) {
-               if(countMalfunctioningNeighbors(i,j) >1){
-                   int[] addThis=new int[] {i,j};
-                   result.add(addThis);
-               }
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public String toString(){
-        String result="";
-        for (int i = 0; i < cityGrid.length; i++) {
-            for (int j = 0; j < cityGrid[0].length; j++) {
-                result+=Integer.toString(cityGrid[i][j])+"    ";
-            }
-            result+="\n";
-        }
-        return result;
-    }
-
 }
